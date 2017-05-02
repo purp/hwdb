c3d709f328e136b58b979f4a58c7dc0d5fc6c222
Jim Meyer <jim@geekdaily.org>
2017-05-01 18:42:43 -0700

    Improved adjustments
    Use `create or replace`

--- db/sql/adjustments.sql
+++ db/sql/adjustments.sql
@@ -13,25 +13,19 @@ alter table user modify id int not null auto_increment primary key;
 -- alter table environment modify id int not null auto_increment;
 -- alter table envmach add CONSTRAINT `environment_fk` FOREIGN KEY (`envid`) REFERENCES `environment` (`id`);
 
-drop view users;
-drop view environments;
-drop view machines;
-drop view environments_users;
-drop view environments_machines;
-
-create
+create or replace
   sql security invoker
   view machines as (select * from provodc);
 
-create
+create or replace
   sql security invoker
   view users as (select * from user);
 
-create
+create or replace
   sql security invoker
   view environments as (select * from environment);
 
-create
+create or replace
   sql security invoker
   view environments_users as (
     select envid as environment_id, userid as user_id from envuser
@@ -39,7 +33,7 @@ create
 
 
 -- setting this up as a habtm even though a machine should belong to one env
-create
+create or replace
   sql security invoker
   view environments_machines as (
     select e.envid as environment_id, m.id as machine_id from envmach e inner join machines m on e.serial = m.serial
